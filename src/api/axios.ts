fecf28ccdfaf0584f7ede8c9f399ca001c41d09e
cleankim <112611552+cleankim@users.noreply.github.com>
2023-03-02 11:05:03 +0900

    [sto-26] 포트 설정 수정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,7 +4,6 @@ const instance = axios.create ({
     headers: {
         'Access-Control-Allow-Origin': 'http://192.168.252.47:8888'	// 서버 domain
     },
-    baseURL: 'http://localhost:3000',
     withCredentials: true
 })
 
